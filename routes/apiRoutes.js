051d46de97f8d65aaeb995b00bb484777cdda67d
Patience D <patience.deaton2@outlook.com>
2019-06-14 16:42:21 -0400

    added api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,42 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/create-plan", function(req, res) {
-    db.Plan.findAll({
-      // where: {
-      //   date: req.body.date,
-      //   park: req.body.park,
-      // }
-    }).then(function(dbResponse) {
-      // waitTimes = [];
-      // for (i=0; i < dbResponse.length; i++) {
-      // if (dbResponse[i].waitTime < 30) {
-        // dbResponse[i].waitTimes.push(waitTimes);
-        // console.log(waitTimes);
-      // }
-      // }
-      res.json(dbResponse);
+  app.get("/api/create-plan/", function (req, res) {
+    db.Plan.findAll({}).then(function (data) {
+      res.json(data);
+    });
+  });
+
+  app.get("/api/park", function (req, res) {
+    db.Park.findAll({}).then(function (data) {
+      res.json(data);
+    });
+  });
+
+  app.get("/api/rides", function (req, res) {
+    db.Rides.findAll({}).then(function (data) {
+      res.json(data);
+    });
+  });
+
+  app.get("/api/user", function (req, res) {
+    db.User.findAll({}).then(function (data) {
+      res.json(data);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.post("/api/user", function (req, res) {
+  //   db.User.create(req.body).then(function (dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function() {
-      res.json(dbExample);
-    });
-  });
+  //   app.delete("/api/examples/:id", function (req, res) {
+  //     db.Example.destroy({ where: { id: req.params.id } }).then(function () {
+  //       res.json(dbExample);
+  //     });
+  //   });
 };
