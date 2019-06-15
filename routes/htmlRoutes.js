bc2a07db64102d7cdea934e8713d8645f63b4655
huntsimon23 <hunt.simon23@gmail.com>
2019-06-15 06:04:01 -0400

    handlebars updates to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,6 +8,12 @@ module.exports = function (app) {
       });
     });
 
+    app.get("/example", function(req, res) {
+      res.render("example", {
+        style: "example.css"
+      });
+    });
+
   // Load user page after login
   // Find all from Park model
   app.get("/user", function(req, res) {
@@ -23,20 +29,17 @@ module.exports = function (app) {
 // $.get() find all where park = (parkname from user click)
   //respond with rides from Ridenames table
 
-// $.post each time user clicks on a ride
-  //post to "timeslot" via dropdown menu
-  //
-
-// $.post to save the plans
-
   // Build active user plan
-  app.get("/user/create-plan/:id", function(req, res) {
-    db.Plan.findAll({
-      where: {id: req.params.id}
-    }).then(function() {
+  app.get("/user/create-plan/", function(req, res) {
+    db.Rides.findAll({
+      where: {park: "Magic Kingdom"}
+      //"park" is field data submitted by user on button click - replace this
+    }).then(function(dbData) {
       res.render("create-plan", {
-        style: "create-plan.css"
+        style: "create-plan.css",
+        ride: dbData
       });
+      console.log(dbData);
     });
   });
 
