6f7beb3ca590f67c73d7d42b8ab02c523eaeba5a
huntsimon23 <hunt.simon23@gmail.com>
2019-06-17 21:22:19 -0400

    Users <-- updated

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-    var Users = sequelize.define("User", {
+    var Users = sequelize.define("Users", {
         userName: DataTypes.STRING,
         password: DataTypes.STRING,
     });
